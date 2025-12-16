4d965176379f6a180bca525c6cbee201af2cf048
Arif-kemal <arifkemalseremet@gmail.com>
2025-12-16 17:00:10 +0300

    Time: 4 ms (92.33%), Space: 64 MB (96.08%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -4,7 +4,8 @@ function isPalindrome(x: number): boolean {
     let reversedHalf=0;
     while(x>reversedHalf){
         reversedHalf=reversedHalf*10+(x%10);
-        x=Math.floor(x/10);
+        x=(x/10) |0;
     }
-    return x===reversedHalf||x===Math.floor(reversedHalf/10);
+    return x===reversedHalf||x===((reversedHalf/10)|0);
+    //math cagirmak maliyetli
 };
