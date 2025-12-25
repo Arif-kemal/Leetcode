2b6499671c924223f2b497ce62232f8374bfbc0e
Arif-kemal <arifkemalseremet@gmail.com>
2025-12-25 23:11:38 +0300

    Time: 0 ms (100%), Space: 13.5 MB (76.02%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
+            }else if(nums[nums.size()-1]<target){
+                return nums.size();
+            }else if(nums[i]>target){
+                return i;     
+            }
+        }
+        return 0;
+    }
+};
