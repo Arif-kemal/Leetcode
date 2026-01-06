df03449919710ea96686c089508101e05843a7de
Arif-kemal <arifkemalseremet@gmail.com>
2026-01-06 19:25:26 +0300

    Time: 253 ms (11.44%), Space: 12.6 MB (64.11%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        /*bu soruda bizden 1 den n'e kadar olan asal sayilarin adetini istiyor
+        zor gibi gozuksede gayet basit Run time eror almak en buyuk sorun ama
+        kurdugum algoritma bunu asiyor*/
         if(n<=2)return 0;
-        vector<bool>arr(n,true);
-
+        vector<bool>arr(n,true);//vector olusturuyor ve hepsini true diyoruz
         for(int i=0;i<n;i++){
             arr[i]=true;
         }
-
         for(int i=2;i*i<=n;i++){
-            if(arr[i]){
-                for(int j=i;j*i<n;j++){
-                    arr[i*j]=false;
-                }
-            }
+            if(arr[i]){for(int j=i;j*i<n;j++){arr[i*j]=false;}}
         }
         int k=0;
         for(int i=2;i<n;i++){
