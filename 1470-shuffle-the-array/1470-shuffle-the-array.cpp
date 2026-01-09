4cafd2ed4a1d166c5ed9cc37bb0647f578edaafe
Arif-kemal <arifkemalseremet@gmail.com>
2026-01-09 15:44:50 +0300

    Time: 5 ms (23.3%), Space: 13.6 MB (16.28%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2*n);
+        for(int i=0;i<2*n;i++){
+            if(i%2){
+                ans[i]=nums[n+i/2];
+            }else{ans[i]=nums[i/2];}
+        }
+        return ans;
+    }
+};
