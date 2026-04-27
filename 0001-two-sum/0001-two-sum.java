ed0337843c248fbd97a537cfd2201d2f78b2547a
Arif-kemal <arifkemalseremet@gmail.com>
2026-04-28 00:56:14 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,17 @@
-
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Sayıları ve indislerini tutacak bir "not defteri" (HashMap)
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i]; // Hedefe ulaşmak için gereken eksik sayı
-            
-            // "Eksik sayı daha önce notlarım arasına girmiş mi?"
-            if (map.containsKey(complement)) {
-                // Evet girmiş! O zaman o sayının sırasını ve şu anki sırayı döndür.
-                return new int[] { map.get(complement), i };
+        int[] dizi= new int[2];
+        int n=0;
+        for(int i=0;i<nums.length-1;i++){
+            n=nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                if(target-n==nums[j]){  
+                    dizi[0]=i;
+                    dizi[1]=j;
+                    return dizi;
+                }
             }
-            
-            // Eğer yoksa, şu anki sayıyı ve sırasını deftere kaydet.
-            map.put(nums[i], i);
         }
-        
-        return new int[] {}; // Buraya hiç gelmeyecek
+        return dizi;
     }
 }
