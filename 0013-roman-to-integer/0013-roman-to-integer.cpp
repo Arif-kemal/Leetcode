57bf0703a5f85c40d0e7862b7998f577038872bb
Arif-kemal <arifkemalseremet@gmail.com>
2026-09-18 21:26:14 +0300

    Time: 3 ms (74.81%), Space: 10.2 MB (77.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,56 +1,27 @@
 class Solution {
 public:
+int degeral(char c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default:  return 0;
+    }
+}
     int romanToInt(string s) {
-        unordered_map<char, int> roman = {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i+1<s.length() && roman[s[i]]<roman[s[i+1]]) {
-                result -= roman[s[i]];
-            } else {
-                result += roman[s[i]];
+        int toplam=0;
+        for(int i=0;i<s.length();i++){
+            int suan=degeral(s[i]);
+            if(i+1<s.length() && suan<degeral(s[i+1])){
+                toplam-=suan;
+            }else{
+                toplam+=suan;
             }
         }
-        return result;
-
-
-       /* int num=0;
-        for(int i=0;i<s.size();i++){
-           if(s[i]=='M'){
-             num+=1000;
-           }else if(s[i]=='D' && i+1<=s.size() && s[i+1]!='M'){
-            num+=500;
-           }else if(s[i]=='D' && i+1<=s.size() && s[i+1]=='M'){
-            num-=500;
-           }else if(s[i]=='C' && i+1<=s.size() && s[i+1]!={'M','D'}){
-            num+=100;
-           }else if(s[i]=='C' && i+1<=s.size() && s[i+1]=={'M','D'}
-            num-=100;
-           }else if(s[i]=='L' && i+1<=s.size() && s[i+1]!={'M','D','C'}){
-            num+=50;
-           }else if(s[i]=='L' && i+1<=s.size() && s[i+1]=={'M','D','C'}){
-            num-=50;
-           }else if(s[i]=='X' && i+1<=s.size() && s[i+1]!={'M','D','C','L'}){
-            num+=10;
-           }else if(s[i]=='X' && i+1<=s.size() && s[i+1]=={'M','D','C','L'}){
-            num-=10;
-           }else if(s[i]=='V' && i+1<=s.size() && s[i+1]!={'M','D','C','L','X'}){
-            num+=5;
-           }else if(s[i]=='V' && i+1<=s.size() && s[i+1]=={'M','D','C','L','X'}){
-            num-=5;
-           }else if(s[i]=='I' && i+1<=s.size() && s[i+1]!={'M','D','C','L','X','V'}){
-            num+=1;
-           }else if(s[i]=='I' && i+1<=s.size() && s[i+1]=={'M','D','C','L','X'}]){
-            num-=1;
-           }else{num+=0;}
-        }
-        return num;*/
+        return toplam;
     }
 };
