389685b8deac02193a1236d70a8d52e9ee6252bb
Arif-kemal <arifkemalseremet@gmail.com>
2026-09-20 21:59:40 +0300

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        int toplam=0;
-        for(int i=0;i<s.length();i++){
+        int toplam=0,n=s.length();
+        for(int i=0;i<n;i++){
             toplam+=('z'-s[i]+1)*(i+1);
         }
         return toplam;
