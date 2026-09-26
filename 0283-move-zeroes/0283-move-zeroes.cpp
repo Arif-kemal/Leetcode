c5613da68570b5e95d61e370785133870cf4b81b
Arif-kemal <arifkemalseremet@gmail.com>
2026-09-26 11:22:57 +0300

    Time: 2 ms (15.7%), Space: 24.9 MB (5.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-    vector<int> arr;
-    vector<int> arr2;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]==0) arr.push_back(nums[i]);
-        else arr2.push_back(nums[i]);
-    }
-    arr2.insert(arr2.end(),arr.begin(),arr.end());
-//    for(int k=0;k<nums.size();k++){
-  //      cout<<arr2[k]<<",";
-    //}
-    nums=arr2;
+        vector<int>arif;
+        int n=nums.size(),adet=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)adet++;
+            else{
+                arif.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<adet;i++){
+            arif.push_back(0);
+        }
+        nums=arif;
     }
 };
